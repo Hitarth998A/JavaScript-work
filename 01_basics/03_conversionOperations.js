d4c565c8bb0c8d701cefbee5d2259fd538c25d96
Hitarth998A <hitarth.bhatt2004@gmail.com>
2025-01-19 04:21:53 +0000

    Done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,21 @@
 "use strict";
 let x="33"
 let y=Boolean(x);
-console.log(typeof(y),y);
+//console.log(typeof(y),y);
 
 let a=Boolean("a");
 let b=Boolean("")
-console.log(a,b)
+//console.log(a,b)
+const c=["Hello","Namaskar"]
+let myObj={
+    name:"Hitarth",
+    age:22,
+}
+console.log(myObj);
+
+//function Hello(){
+//     console.log("Hello World")
+//    // z=window.prompt("Enter your age")
+//     console.log("Good to Go")
+// }
+// Hello()
