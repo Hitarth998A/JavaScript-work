b399f14533b8ba1ee21900de7f0985636291fe80
Hitarth998A <hitarth.bhatt2004@gmail.com>
2025-01-20 09:21:10 +0000

    Done

---
+++ 01_basics/03_basics.js
@@ -0,0 +1,40 @@
+// "use strict";
+// let x="33"
+// let y=Boolean(x);
+// //console.log(typeof(y),y);
+
+// let a=Boolean("a");
+// let b=Boolean("")
+// //console.log(a,b)
+// const c=["Hello","Namaskar"]
+// let myObj={
+//     name:"Hitarth",
+//     age:22,
+// }
+// console.log(myObj);
+
+// //function Hello(){
+// //     console.log("Hello World")
+// //    // z=window.prompt("Enter your age")
+// //     console.log("Good to Go")
+// // }
+// // Hello()
+
+// const myObj={
+//     a:"Hello",
+//     b:"World"
+// }
+// One = myObj
+// One.a=("Hii")
+// console.log(myObj.a);
+
+// const One="Hello - Hii"
+// let a=One.split(" ")
+//    console.log(a[1])
+// let a=100000000;
+// console.log(a.toLocaleString('en-IN'));
+// console.log(Math.round(9.8))
+let a = ["Hitarth Bhatt"]
+let b = [3,4]
+const c=[...a,...b,...[null,undefined]]
+console.log(c)
